1300c3f8f7d5539e2187e817201b424c2b891380
andsaki <ruby.on.rails.api@gmail.com>
2026-01-03 20:39:29 +0900

    fix: パスの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://andsaki.github.io/patterns/',
-	base: '/patterns/'
+	site: 'https://andsaki.github.io/design-specialist/',
+	base: '/design-specialist/'
 });
